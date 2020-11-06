06b863c7a8ba1b1bc94d867de8b5efba15d67c29
13828453060 <13828453060@139.com>
2020-11-06 16:51:29 +0800

    第一次提交

---
+++ unlock-music-desktop/MainWindow.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Win32;
+using NetDimension.NanUI;
+using NetDimension.NanUI.Browser;
+using NetDimension.NanUI.HostWindow;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace unlock_music_desktop
+{
+    public class MainWindow : Formium
+    {
+        
+
+        // 设置窗体样式类型
+        public override HostWindowType WindowType => HostWindowType.System;
+        // 指定启动 Url
+        public override string StartUrl => "http://archive.app.local/index.html";
+
+        public MainWindow()
+        {
+            Title = "Unlock Music";
+
+            Subtitle = "音乐解锁";
+
+            // 在此处设置窗口样式
+            Size = new System.Drawing.Size(1024, 768);
+            Mask.BackColor = Color.White;
+            Mask.Image = Properties.Resources.logo;
+            Icon = Tools.ConvertToIcon(Properties.Resources.logo);
+
+            //关闭窗口事件
+            BeforeClose += MainWindow_BeforeClose;
+            //关机事件
+            SystemEvents.SessionEnding += new SessionEndingEventHandler(SystemEvents_SessionEnding);
+        }
+
+        //关机事件
+        private void SystemEvents_SessionEnding(object sender, SessionEndingEventArgs e)
+        {
+            //e.Cancel = true;
+        }
+
+        //关闭窗口事件
+        private void MainWindow_BeforeClose(object sender, FormiumCloseEventArgs e)
+        {
+            //Console.WriteLine("MainWindow_BeforeClose:{0}:{1}",sender.ToString(),e.ToString());
+            
+        }
+
+        protected override void OnReady()
+        {
+            // 在此处进行浏览器相关操作
+            //ShowDevTools();
+            //ExecuteJavaScript("alert('Hello NanUI')");
+
+        }
+
+       
+
+
+    }
+}
---
+++ unlock-music-desktop/Program.cs
@@ -0,0 +1,48 @@
+﻿using NetDimension.NanUI;
+using NetDimension.NanUI.ZippedResource;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace unlock_music_desktop
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+
+            WinFormium.CreateRuntimeBuilder(env =>
+            {
+
+                env.CustomCefSettings(settings =>
+                {
+                    // 在此处设置 CEF 的相关参数
+                });
+
+                env.CustomCefCommandLineArguments(commandLine =>
+                {
+                    // 在此处指定 CEF 命令行参数
+                });
+
+            }, app =>
+            {
+                //注册嵌入ZIP程序集资源处理器
+                app.UseZippedResource("http", "archive.app.local", () => new MemoryStream(Properties.Resources.dist));
+                // 指定启动窗体
+                app.UseMainWindow(context => new MainWindow());
+            })
+            .Build()
+            .Run();
+        }
+    }
+}
